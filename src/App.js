ead23cd2fa3cee52f83f21aef49cffcc18dd670c
Ashesh Vidyut <ashesh.vidyut@gmail.com>
2024-03-10 05:52:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I am batman
+          I am superman
         </a>
       </header>
     </div>
