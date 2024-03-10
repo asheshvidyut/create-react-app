4b1de73104b30081b3c854f55d1db80c6a978ed9
Ashesh Vidyut <ashesh.vidyut@gmail.com>
2024-03-10 18:21:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I am spiderman
+          I am inevitable
         </a>
       </header>
     </div>
